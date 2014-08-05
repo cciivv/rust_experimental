4e117ed7bc0a1276ad0614ef9c8dab9cf2116cf3
Carl Vanderlip <carl.i.vanderlip@gmail.com>
2014-08-05 12:42:48 -0700

    More modules for fizzbuzzers

--- fizzbuzz_bench.rs
+++ fizzbuzz_bench.rs
@@ -5,8 +5,8 @@
 
 extern crate test;
 
-mod fzbz {
-    pub fn rustic_fzbz(num: int) -> String {
+mod rustic {
+    pub fn fizzbuzz(num: int) -> String {
         match ( num % 3 , num % 5) {
             (0, 0) => format!("Fizzbuzz"),
                 (0, _) => format!("Fizz"),
@@ -14,60 +14,69 @@ mod fzbz {
                 (_, _) => num.to_string()
         }
     }
+}
 
-    pub fn cs_fzbz(num: int) -> String {
-        if num % 15 == 0 {
-            format!("Fizzbuzz")
-        } else if num % 3 == 0 {
-            format!("Fizz")
-        } else if num % 5 == 0 {
-            format!("Buzz")
-        } else {
-            num.to_string()
+mod imperative {
+    pub mod simplified {
+        pub fn fizzbuzz(num: int) -> String {
+            if num % 15 == 0 {
+                format!("Fizzbuzz")
+            } else if num % 3 == 0 {
+                format!("Fizz")
+            } else if num % 5 == 0 {
+                format!("Buzz")
+            } else {
+                num.to_string()
+            }
         }
     }
 
-    pub fn c_fzbz(num: int) -> String {
-        if (num % 3 == 0) && (num % 5 ==  0) {
-            format!("Fizzbuzz")
-        } else if num % 3 == 0 {
-            format!("Fizz")
-        } else if num % 5 == 0 {
-            format!("Buzz")
-        } else {
-            num.to_string()
+    pub mod original {
+        pub fn fizzbuzz(num: int) -> String {
+            if (num % 3 == 0) && (num % 5 ==  0) {
+                format!("Fizzbuzz")
+            } else if num % 3 == 0 {
+                format!("Fizz")
+            } else if num % 5 == 0 {
+                format!("Buzz")
+            } else {
+                num.to_string()
+            }
         }
     }
 
-    fn div_by_three(num: int) -> bool {
-        num % 3 == 0
-    }
+    pub mod func_call {
+        fn div_by_three(num: int) -> bool {
+            num % 3 == 0
+        }
 
-    fn div_by_five(num: int) -> bool {
-        num % 5 == 0
-    }
+        fn div_by_five(num: int) -> bool {
+            num % 5 == 0
+        }
 
-    pub fn cfunc_fzbz(num: int) -> String {
-        if div_by_three(num) && div_by_five(num) {
-            format!("Fizzbuzz")
-        } else if div_by_three(num) {
-            format!("Fizz")
-        } else if div_by_five(num) {
-            format!("Buzz")
-        } else {
-            num.to_string()
+        pub fn fizzbuzz(num: int) -> String {
+            if div_by_three(num) && div_by_five(num) {
+                format!("Fizzbuzz")
+            } else if div_by_three(num) {
+                format!("Fizz")
+            } else if div_by_five(num) {
+                format!("Buzz")
+            } else {
+                num.to_string()
+            }
         }
     }
+}
 
-    #[main]
-    fn main() {
-        for i in range(0i, 101) {
-            println!("{}:\t{}\t{}\t{}\t{}", i,
-                                            rustic_fzbz(i),
-                                            c_fzbz(i),
-                                            cfunc_fzbz(i),
-                                            cs_fzbz(i));
-        }
+#[main]
+fn main() {
+    for i in range(0i, 101) {
+        println!("{}:\t{}\t{}\t{}\t{}", i,
+                rustic::fizzbuzz(i),
+                imperative::original::fizzbuzz(i),
+                imperative::func_call::fizzbuzz(i),
+                imperative::simplified::fizzbuzz(i)
+                );
     }
 }
 
@@ -75,24 +84,26 @@ mod fzbz {
 mod testee {
 
     use test::Bencher;
-    use fzbz::{rustic_fzbz, c_fzbz, cfunc_fzbz, cs_fzbz};
+
+    use rustic::fizzbuzz;
+    use imperative::{original, func_call, simplified};
 
     static BENCH_SIZE: int = 4096;
 
 #[bench]
     fn bench_rustic(b: &mut Bencher) {
-        b.iter(|| { for i in range(0, BENCH_SIZE) { rustic_fzbz(i); }});
+        b.iter(|| { for i in range(0, BENCH_SIZE) { fizzbuzz(i); }});
     }
 #[bench]
     fn bench_c(b: &mut Bencher) {
-        b.iter(|| { for i in range(0, BENCH_SIZE) { c_fzbz(i); }});
+        b.iter(|| { for i in range(0, BENCH_SIZE) { original::fizzbuzz(i); }});
     }
 #[bench]
     fn bench_cfunc(b: &mut Bencher) {
-        b.iter(|| { for i in range(0, BENCH_SIZE) { cfunc_fzbz(i); }});
+        b.iter(|| { for i in range(0, BENCH_SIZE) { func_call::fizzbuzz(i); }});
     }
 #[bench]
     fn bench_cs(b: &mut Bencher) {
-        b.iter(|| { for i in range(0, BENCH_SIZE) { cs_fzbz(i); }});
+        b.iter(|| { for i in range(0, BENCH_SIZE) { simplified::fizzbuzz(i); }});
     }
 }
